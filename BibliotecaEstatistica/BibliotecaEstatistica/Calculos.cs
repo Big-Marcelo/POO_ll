515708316bf04e06a5a07463385c85ee38dbd4d5
Marcelo <140878769+Big-Marcelo@users.noreply.github.com>
2025-08-28 15:00:32 -0300

    Roteiro de Aula l (Exercício 4)

---
+++ BibliotecaEstatistica/BibliotecaEstatistica/Calculos.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace BibliotecaEstatistica
+{
+    public class Calculos
+    {
+        public double CalcularMedia(double[] numeros)
+        {
+            if (numeros == null || numeros.Length == 0)
+                return 0;
+            return numeros.Average();
+        }
+
+        public double CalcularSoma(double[] numeros)
+        {
+            if (numeros == null)
+                return 0;
+            return numeros.Sum();
+        }
+    }
+}
